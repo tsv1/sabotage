3733cfdca15f418a0f25d0b5a5335391a0d323a3
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2019-03-17 13:05:48 +0700

    update tests

--- tests/test_disconnected.py
+++ tests/test_disconnected.py
@@ -7,21 +7,22 @@ from sabotage import disconnected
 
 
 class TestDisconnected(unittest.TestCase):
-    IMAGE_ALPINE = "alpine:3.9.2"
-    COMMAND_WAIT = "sh -c 'trap : TERM INT; sleep 3600 & wait'"
-    COMMAND_PING = "sh -c 'ping -q -c 1 -w 1 {host} > /dev/null && echo -n OK || echo -n FAIL'"
-
     def _create_network(self, name=None, driver="bridge"):
-        name = name or str(uuid4())
-        return self.client.networks.create(name, driver=driver)
+        network = self.client.networks.create(name if name else str(uuid4()), driver=driver)
+        self.networks += [network]
+        return network
 
-    def _create_container(self, network, name=None, image=IMAGE_ALPINE, command=COMMAND_WAIT, labels=None):
-        name = name or str(uuid4())
-        labels = labels or {}
-        return self.client.containers.create(image, name=name, network=network, command=command, labels=labels)
+    def _create_container(self, network, name=None, labels=None):
+        container = self.client.containers.create("alpine:3.9.2",
+                                                  name=name if name else str(uuid4()),
+                                                  network=network,
+                                                  command="sh -c 'trap : TERM INT; sleep 3600 & wait'",
+                                                  labels=labels if labels else {})
+        self.containers += [container]
+        return container
 
     def _ping(self, container, host):
-        command = self.COMMAND_PING.format(host=host)
+        command = f"sh -c 'ping -q -c 1 -w 1 {host} > /dev/null && echo -n OK || echo -n FAIL'"
         result = container.exec_run(command)
         if isinstance(result, bytes):
             return result.decode()
@@ -29,20 +30,20 @@ class TestDisconnected(unittest.TestCase):
 
     def setUp(self):
         self.client = docker.from_env()
-        self.network = self._create_network()
+        self.networks = []
         self.containers = []
 
     def tearDown(self):
         for container in self.containers:
             container.stop()
-        self.network.remove()
+        for network in self.networks:
+            network.remove()
         self.client.api.close()
 
     def test_disconnected(self):
-        container1 = self._create_container(network=self.network.id)
-        container2 = self._create_container(network=self.network.id)
-        self.containers += [container1]
-        self.containers += [container2]
+        network = self._create_network()
+        container1 = self._create_container(network.id)
+        container2 = self._create_container(network.id)
         container1.start()
         container2.start()
 
@@ -57,15 +58,14 @@ class TestDisconnected(unittest.TestCase):
         self.assertEqual(res, "OK")
 
     def test_disconnected_with_compose_labels(self):
+        network = self._create_network()
         service_name, project_name = str(uuid4()), str(uuid4())
-        container1 = self._create_container(name=service_name, network=self.network.id, labels={
+        container1 = self._create_container(name=service_name, network=network.id, labels={
             "com.docker.compose.project": project_name,
             "com.docker.compose.service": service_name,
             "com.docker.compose.container-number": "1",
         })
-        container2 = self._create_container(network=self.network.id)
-        self.containers += [container1]
-        self.containers += [container2]
+        container2 = self._create_container(network=network.id)
         container1.start()
         container2.start()
 
