dd8a3053aa8dd168afcd1bbf5534288eec508071
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2019-03-17 15:26:19 +0700

    bump version

--- sabotage/__init__.py
+++ sabotage/__init__.py
@@ -6,4 +6,4 @@ sabotaged = Disconnected
 
 
 __all__ = ("sabotaged", "disconnected", "Disconnected")
-__version__ = "0.1.0"
+__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="sabotage",
-    version="0.1.0",
-    description="",
+    version="0.2.0",
+    description="Infra-tests made easy",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Nikita Tsvetkov",
