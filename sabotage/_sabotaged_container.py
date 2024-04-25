9f33ffef5bebb82c018703be1628c502f71cb46a
Nikita Tsvetkov <tsv1@fastmail.com>
2024-04-25 23:55:08 +0400

    fix context manager

--- sabotage/_sabotaged_container.py
+++ sabotage/_sabotaged_container.py
@@ -15,12 +15,17 @@ async def sabotaged_container(service_name: str,
                               docker_client_factory: Callable[[], DockerClient] = DockerClient
                               ) -> AsyncGenerator[None, None]:
     docker_client = docker_client_factory()
+
+    container = None
     try:
         container = await find_container(docker_client,
                                          service_name=service_name,
                                          project_name=project_name)
         await stop_container(docker_client, container)
         yield
-        await start_container(docker_client, container)
     finally:
-        await docker_client.close()
+        try:
+            if container:
+                await start_container(docker_client, container)
+        finally:
+            await docker_client.close()
